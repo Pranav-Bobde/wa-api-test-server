4cc50b29284e636de168bab8b8e6a0da5d237ce9
Pranav Bobde <bobdep31@gmail.com>
2022-12-12 04:12:55 +0530

    first comming with get, post cb endpoints

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const parser = require("body-parser");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/cb", (req, res) => {
+  console.log("REQ: ", req);
+  console.log("BODY: ", req.body);
+  return
+});
+
+app.post("/cb", (req, res) => {
+  console.log("REQ: ", req);
+  console.log("BODY: ", req.body);
+  return
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
