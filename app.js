24b96853f5588c421a0c63070a2de79c906db9e8
Pranav Bobde <bobdep31@gmail.com>
2022-12-12 07:07:48 +0530

    chore: add logs to debug post body

--- app.js
+++ app.js
@@ -10,28 +10,22 @@ app.get("/", (req, res) => {
 });
 
 app.get("/cb", (req, res) => {
-  // console.log("REQ: ", req);
   let mode = req.query["hub.mode"];
-  console.log("Mode: ", mode);
   let token = req.query["hub.verify_token"];
-  console.log("Token: ", token);
   let challenge = req.query["hub.challenge"];
-  console.log("Challenge: ", challenge);
   if (mode && token) {
-    console.log("mode: ", mode)
     if (mode === "subscribe" && token === "test") {
-      console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
     } else {
       res.sendStatus(403);
     }
   }
-  console.log("end")
 });
 
 app.post("/cb", (req, res) => {
   console.log("REQ: ", req);
   console.log("BODY: ", req.body);
+  console.log("BODY ENTRY: ", JSON.stringify(req.body.entry));
   res.send("CB POST");
 });
 
