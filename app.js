8baf1f67afa37e79d2c89c22b0cbfcd575f2ca30
Pranav Bobde <bobdep31@gmail.com>
2022-12-30 22:32:40 +0530

    add async reply func, add cb res 200 cb finally, on conflict do nothing

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express();
 const https = require("https");
 const Pool = require("pg").Pool;
 const jwt = require("jsonwebtoken");
+const axios = require("axios");
 require("dotenv").config();
+
+let count = 0;
 
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
@@ -14,56 +17,100 @@ const pool = new Pool({
   database: process.env.POSTGRES_DB,
 });
 
-function replyMessage(number, token) {
-  const options = {
-    hostname: "graph.facebook.com",
-    path: "/v15.0/108004132170880/messages",
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.ACCESS_TOKEN}}`,
-      "Content-Type": "application/json",
-    },
-  };
+// async function replyMessage(number, token) {
+//   console.log("in reply");
+//   const options = {
+//     hostname: "graph.facebook.com",
+//     path: "/v15.0/108004132170880/messages",
+//     method: "POST",
+//     headers: {
+//       Authorization: `Bearer ${process.env.ACCESS_TOKEN}}`,
+//       "Content-Type": "application/json",
+//     },
+//   };
 
-  const req = https.request(options, (res) => {
-    console.log(`statusCode: ${res.statusCode}`);
+//   return new Promise((resolve, reject) => {
+//     console.log("in promise");
+//     const req = https.request(options, (res) => {
+//       res.on("data", (d) => {
+//         console.log("in data");
+//         resolve(d);
+//       });
+//     });
 
-    res.on("data", (d) => {
-      process.stdout.write(d);
-    });
-  });
+//     req.on("error", (error) => {
+//       reject(error);
+//     });
 
-  req.on("error", (error) => {
-    console.error(error);
-  });
+//     req.write(
+//       JSON.stringify({
+//         messaging_product: "whatsapp",
+//         to: `${number}`,
+//         type: "template",
+//         template: {
+//           name: "sample_shipping_confirmation",
+//           language: {
+//             code: "en_US",
+//           },
+//           components: [
+//             {
+//               type: "body",
+//               parameters: [
+//                 {
+//                   type: "text",
+//                   text: `https://wa-api-test-server.onrender.com/whatsapp?data=${token}`,
+//                 },
+//               ],
+//             },
+//           ],
+//         },
+//       })
+//     );
 
-  req.write(
-    JSON.stringify({
-      messaging_product: "whatsapp",
-      to: `${number}`,
-      type: "template",
-      template: {
-        name: "sample_shipping_confirmation",
-        language: {
-          code: "en_US",
-        },
-        components: [
-          {
-            type: "body",
-            parameters: [
-              {
-                type: "text",
-                text: `https://wa-api-test-server.onrender.com/whatsapp?data=${token}`,
-              },
-            ],
+//     req.end();
+//   });
+// }
+
+async function replyMessage(number, token) {
+  try {
+    const response = await axios.post(
+      "https://graph.facebook.com/v15.0/108004132170880/messages",
+      {
+        messaging_product: "whatsapp",
+        to: `${number}`,
+        type: "template",
+        template: {
+          name: "sample_shipping_confirmation",
+          language: {
+            code: "en_US",
           },
-        ],
+          components: [
+            {
+              type: "body",
+              parameters: [
+                {
+                  type: "text",
+                  text: `https://wa-api-test-server.onrender.com/whatsapp?data=${token}`,
+                },
+              ],
+            },
+          ],
+        },
       },
-    })
-  );
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  req.end();
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
@@ -86,57 +133,85 @@ app.get("/cb", (req, res) => {
 });
 
 app.post("/cb", async (req, res) => {
+  if(count == 2){
+    res.sendStatus(200);
+    return;
+  }
+  count++;
   const body_param = req.body;
+
   console.log("BODY: ", body_param);
   console.log("CHANGES: ", body_param.entry[0].changes[0]);
+  console.log(
+    "Statuses: ",
+    JSON.stringify(body_param.entry[0].changes[0].value.statuses)
+  );
+
   if (body_param.object) {
     if (
       body_param.entry &&
-      body_param.entry[0].changes &&
-      body_param.entry[0].changes[0]["value"].messages
+      body_param.entry[0].changes
     ) {
-      console.log(
-        "Number: ",
-        body_param.entry[0].changes[0]["value"].messages[0].from
-      );
-
-      const name = body_param.entry[0].changes[0]["value"].contacts[0].profile.name;
-      const phone = body_param.entry[0].changes[0]["value"].messages[0].from;
-
-      console.log(
-        "Message: ",
-        body_param.entry[0].changes[0]["value"].messages[0].text.body
-      );
-      
+      let user = null;
       try {
-        // create a new user in the database
-        await pool.query(
-          `
-          INSERT INTO "customer"."users" ( "name", "phone" ) 
-          VALUES ($1, $2)
-          `,
-          [name, phone]
-        );
+        if (body_param.entry[0].changes[0]["value"].messages) {
+          console.log(
+            "Number: ",
+            body_param.entry[0].changes[0]["value"].messages[0].from
+          );
+  
+          const name =
+            body_param.entry[0].changes[0]["value"].contacts[0].profile.name;
+          const phone = body_param.entry[0].changes[0]["value"].messages[0].from;
 
-        // get the user from the database
-        const user = (await pool.query(
-          `
-          SELECT * FROM "customer"."users" WHERE "phone" = $1 AND "is_deleted" = false
-          `,
-          [phone]
-        )).rows[0];        
+          console.log("Name: ", name);
+          console.log("Phone: ", phone);
+  
+          console.log(
+            "Message: ",
+            body_param.entry[0].changes[0]["value"].messages[0].text.body
+          );
+
+          // insert if not exists & get the user from the database
+          user = (
+            await pool.query(
+              `
+              INSERT INTO "customer"."users" ("name", "phone")
+              VALUES ($1, $2)
+              ON CONFLICT ("phone") DO UPDATE SET "name" = excluded."name"
+              RETURNING *
+              `,
+              [name, phone]
+            )
+          ).rows[0];
+          console.log("USER1: ", user);
+        } else {
+          // get the user from the database
+          user = (
+            await pool.query(
+             `
+              SELECT * FROM "customer"."users" WHERE "phone" = $1 
+            `,
+            [body_param.entry[0].changes[0].value.statuses[0].recipient_id]
+            )
+          ).rows[0];
+
+          console.log("USER2: ", user);
+        }
+
+        // generate a token
+        const token = jwt.sign({ id: user.id }, process.env.SECRET, {
+          expiresIn: "1m",
+        });
+
+        // send a message to the user with token
+        await replyMessage(
+          body_param.entry[0].changes[0]["value"].messages ? body_param.entry[0].changes[0]["value"].messages[0].from : body_param.entry[0].changes[0].value.statuses[0].recipient_id,
+          token
+        );
       } catch (error) {
         console.log("ERROR: ", error);
-      }
-
-      // generate a token
-      const token = jwt.sign({ id: user.id }, process.env.SECRET, {
-        expiresIn: "1m",
-      });
-
-      // send a message to the user with token
-      replyMessage(body_param.entry[0].changes[0]["value"].messages[0].from, token);
-
+      } 
       res.sendStatus(200);
     }
   }
